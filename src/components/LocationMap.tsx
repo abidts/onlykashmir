8f6441934f3296ab13aa0c70bf6ede6ae9fadbef
abidts <aabidshora@gmail.com>
2026-07-14 23:35:36 +0530

    new map

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -15,7 +15,7 @@ export default function LocationMap() {
           <div className="rounded-2xl overflow-hidden shadow-xl border border-slate-800 bg-slate-900">
             <div className="aspect-video w-full">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26482.84759067877!2d82.7325294!3d21.0680074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa23b3da6dc1fa2f7:0xace6b98d717bcfbc!2sOnly+Kashmir+Tour+%26+Travels!5e0!3m2!1sen!2sin!4v1720584000000!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.123456789!2d74.7704696!3d34.0724249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e19195fb25adc1:0xb12120aa2234401a!2sOnly+Kashmir+Tour+%26+Travels!5e0!3m2!1sen!2sin!4v1720584000000!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
